6570e9b0fc760262734ce1f0c1d234fffad9af23
pranjalsorte <pranjalsorte.8305@gmail.com>
2026-05-20 11:20:28 +0530

    added slicing hw

---
+++ may19hw.py
@@ -0,0 +1,11 @@
+n="Pranjal Amar Sorte"
+print(len(n))
+
+print(n[0:7:+1])    # Pranjal
+print(n[6:-19:-1])   #lajnarP
+
+print(n[-7:-11:-1])  #ramA
+print(n[8:12])  #Amar
+
+print(n[13:18])   #Sorte
+print(n[-1:-6:-1])  #etroS
