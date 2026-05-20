898ffdd34b3bd376cc4bb696238f8e37ea250c86
pranjalsorte <pranjalsorte.8305@gmail.com>
2026-05-20 20:56:34 +0530

    added for loop hw

---
+++ may20hw.py
@@ -0,0 +1,6 @@
+"""s="Instagram"
+count=0
+for ch in s:
+    if ch=="a" or ch=="e" or ch=="i" or ch=="o" or ch=="u" or ch=="A" or ch=="E" or ch=="I" or ch=="O" or ch=="U":
+        count=count+1
+print("Vowels in this string are:",count)"""
