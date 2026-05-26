bfbdb5c0d4d1b36571c29801909f9f956d8a15e1
pranjalsorte <pranjalsorte.8305@gmail.com>
2026-05-26 13:03:30 +0530

    added may 25th hw

---
+++ may25hw.py
@@ -0,0 +1,6 @@
+# Find minimumn maximum and average salary of employee
+salary=[67000,45000,78000,55000,28000,44000,33000]
+avg=sum(salary)//len(salary)
+print("Minimun Salary is:",min(salary))
+print("Maximum Salary is:",max(salary))
+print("Average Salary is:",avg)
